b6f9a1ce420d51387da7c1a4261d764fcb6f34b8
gebrina <gsonofmika@gmail.com>
2023-12-29 01:43:11 +0300

    create Expense Model

---
+++ Models/Expense.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExpenseTracker.Models{
+    public class Expense{
+        public string Description {get;set;}="";
+        public double Amount {get;set;}
+        public int ID {get;set;}
+
+        public List<Expense> Expenses = [];
+
+        public List<Expense> GetExpenses(){
+            return Expenses;
+        }
+
+        public void AddExpense(Expense expense){
+            Expenses.Add(expense);
+        }
+
+        public void UpdateExpense(Expense expense,int ID){
+         var expenseTobeUpdated = Expenses.Where(expense=>expense.ID==ID);
+        }
+      
+        public void DeleteExpense(int ID){
+         var expenseTobeDeleted = Expenses.Where(expense=>expense.ID==ID);
+        }
+        
+    }
+}
