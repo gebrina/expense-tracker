f8b9faaaad6fb23bc95dc1b8f5ab1a42783cdb10
gebrina <gsonofmika@gmail.com>
2023-12-29 15:52:54 +0300

    add navliks active status

---
+++ wwwroot/js/script.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.addEventListener("load", () => {
+  const urlPath = location.pathname;
+  let activeLink = null;
+  if (urlPath === "/") {
+    activeLink = document.querySelector("#home");
+  } else {
+    activeLink = document.querySelector("#expense");
+  }
+  activeLink.classList.add("active");
+});
